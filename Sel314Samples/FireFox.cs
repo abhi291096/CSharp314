332dc5e955078f7c40e7b8d4110e6893f849f31b
abhishek mordani <abhishekmordani@abhisheks-MacBook-Pro.local>
2018-10-10 19:31:28 +0530

    Initial Comit

---
+++ Sel314Samples/FireFox.cs
@@ -0,0 +1,48 @@
+﻿
+
+//using System;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Remote;
+//using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Safari;
+//using OpenQA.Selenium.Firefox;
+//using OpenQA.Selenium.IE;
+
+
+
+//namespace SeleniumTest
+//{
+//    class Safari
+//    {
+//        //static InternetExplorerOptions IEoptions;
+
+//        static void Main(string[] args)
+//        {
+//            IWebDriver driver;
+
+
+
+
+//            FirefoxOptions options = new FirefoxOptions();
+
+//            //options.AddAdditionalCapability(CapabilityType.Version, "68", true);
+//            options.AddAdditionalCapability("os_version", "Sierra", true);
+//            options.AddAdditionalCapability("os", "OS X", true);
+//            options.AddAdditionalCapability("browser_version", "60.0", true);
+
+//            driver = new RemoteWebDriver(new Uri("http://<your_username>:<your_access_key>@hub-cloud.browserstack.com/wd/hub/"),
+//                                         options.ToCapabilities());
+
+//            driver.Navigate().GoToUrl("http://www.google.com");
+//            Console.WriteLine(driver.Title);
+
+//            IWebElement query = driver.FindElement(By.Name("q"));
+//            query.SendKeys("Browserstack");
+//            query.Submit();
+//            Console.WriteLine(driver.Title);
+
+//            driver.Quit();
+//        }
+//    }
+//}
+
